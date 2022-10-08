96bda0ce09773a11427a550d55c733efadb8a4fd
girishhiremath <girishhiremath@heptagon.in>
2022-10-08 17:24:23 +0530

    implemented checkpoint callback

--- src/training.py
+++ src/training.py
@@ -1,6 +1,8 @@
+import os
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
-from src.utils.model import create_model
+from src.utils.model import create_model, save_model
+from src.utils.callbacks import get_callbacks
 import argparse
 
 def training(config_path):
@@ -19,8 +21,21 @@ def training(config_path):
     EPOCHS = config["params"]["epochs"]
     VALIDATION_SET = (X_valid, y_valid)
 
+     # create callbacks
+    CALLBACK_LIST = get_callbacks(config, X_train)
+
     history = model.fit(X_train, y_train, epochs=EPOCHS,
-                    validation_data=VALIDATION_SET)
+                    validation_data=VALIDATION_SET, callbacks=CALLBACK_LIST)
+
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_dir = config["artifacts"]["model_dir"]
+    
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+    
+    model_name = config["artifacts"]["model_name"]
+
+    save_model(model, model_name, model_dir_path)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
